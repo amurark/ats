70cfc704e4c304e5207b92930e089c15ffe58734
Ankit <amurark@ncsu.edu>
2016-10-21 06:16:07 -0400

    floating title

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ setInterval(function(){
         carousal[i].style.left = (-100*count) + "%";
     }
 },10000);
+
+var count1 = 0
+var count2 = 0
+$(window).on('scroll',function(e){
+  var sTop = $(window).scrollTop();
+  if(count1 == 0 && sTop > 0) {
+    console.log(1);
+    count2 = 0;
+    $(".floatingWords").css({opacity: '1'});
+    $("#firstWord").css({ top: '0px' });
+    $("#secondWord").css({ top: '0px' });
+    $("#thirdWord").css({ top: '0px' });
+    count1++;
+  }
+  if(count2 == 0 && sTop == 0) {
+    console.log(2);
+    count1 = 0;
+    $("#firstWord").css({ top: '50px' });
+    $("#secondWord").css({ top: '200px' });
+    $("#thirdWord").css({ top: '350px' });
+    $(".floatingWords").css({opacity: '0'});
+    count2++;
+  }
+});
